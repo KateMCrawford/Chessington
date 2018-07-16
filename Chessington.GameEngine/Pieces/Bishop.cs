d3c7a18bcd1bad0d14d3ba729dbd8e4888cb31cc
Katherine Crawford <Katherine.Crawford@softwire.com>
2018-07-16 11:53:28 +0100

    Red-04 passed - bishop moves added

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,29 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var availableMoves = new List<Square>();
+            var differenceCol = 0;
+            var pos = 0;
+
+            for (int i = 0; i < GameSettings.BoardSize; i++)
+            {
+                differenceCol = currentSquare.Col - i;
+                pos = currentSquare.Row - differenceCol;
+                if (i != currentSquare.Col && pos < GameSettings.BoardSize)
+                {
+                    availableMoves.Add(new Square(pos, i));
+                }
+
+                pos = currentSquare.Row + differenceCol;
+                if (i != currentSquare.Col && pos < GameSettings.BoardSize)
+                {
+                    availableMoves.Add(new Square(pos, i));
+                }
+
+            }
+
+            return availableMoves;
         }
     }
 }
