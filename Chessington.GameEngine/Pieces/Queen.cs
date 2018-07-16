a2e5186bad200bc1032467e9c7ac5a0b002edc00
Katherine Crawford <Katherine.Crawford@softwire.com>
2018-07-16 11:56:28 +0100

    Red-05 passed - queen moves added

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,42 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var availableMoves = new List<Square>();
+            var differenceCol = 0;
+            var pos = 0;
+
+            for (int i = 0; i < GameSettings.BoardSize; i++)
+            {
+                differenceCol = currentSquare.Col - i;
+                pos = currentSquare.Row - differenceCol;
+                if (i != currentSquare.Col && pos < GameSettings.BoardSize)
+                {
+                    availableMoves.Add(new Square(pos, i));
+                }
+
+                pos = currentSquare.Row + differenceCol;
+                if (i != currentSquare.Col && pos < GameSettings.BoardSize)
+                {
+                    availableMoves.Add(new Square(pos, i));
+                }
+
+            }
+
+            for (int i = 0; i < GameSettings.BoardSize; i++)
+            {
+                if (i != currentSquare.Col)
+                {
+                    availableMoves.Add(new Square(currentSquare.Row, i));
+                }
+
+                if (i != currentSquare.Row)
+                {
+                    availableMoves.Add(new Square(i, currentSquare.Col));
+                }
+            }
+
+            return availableMoves;
         }
     }
 }
