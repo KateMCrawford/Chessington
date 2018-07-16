db83a74a4a132d7f0091e36494479d9a42568b40
Katherine Crawford <Katherine.Crawford@softwire.com>
2018-07-16 13:28:13 +0100

    Red-10 passed - bishops blocked

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -12,27 +12,31 @@ namespace Chessington.GameEngine.Pieces
         {
             var currentSquare = board.FindPiece(this);
             var availableMoves = new List<Square>();
-            var differenceCol = 0;
-            var pos = 0;
 
-            for (int i = 0; i < GameSettings.BoardSize; i++)
-            {
-                differenceCol = currentSquare.Col - i;
-                pos = currentSquare.Row - differenceCol;
-                if (i != currentSquare.Col && pos < GameSettings.BoardSize)
-                {
-                    availableMoves.Add(new Square(pos, i));
-                }
-
-                pos = currentSquare.Row + differenceCol;
-                if (i != currentSquare.Col && pos < GameSettings.BoardSize)
-                {
-                    availableMoves.Add(new Square(pos, i));
-                }
-
-            }
+            availableMoves = availableMoves
+                .Concat(GetMoves(currentSquare, board, 1, 1))
+                .Concat(GetMoves(currentSquare, board, 1, -1))
+                .Concat(GetMoves(currentSquare, board, -1, 1))
+                .Concat(GetMoves(currentSquare, board, -1, -1))
+                .ToList();
 
             return availableMoves;
+        }
+
+        private IEnumerable<Square> GetMoves(Square currentSquare, Board board, int rowDirection, int colDirection)
+        {
+            var moves = new List<Square>();
+
+            int i = 1;
+            Square nextMove = new Square(currentSquare.Row + i * rowDirection, currentSquare.Col + i * colDirection);
+            while (i < GameSettings.BoardSize && board.IsSquareEmpty(nextMove))
+            {
+                moves.Add(nextMove);
+                i++;
+                nextMove = new Square(currentSquare.Row + i * rowDirection, currentSquare.Col + i * colDirection);
+            }
+
+            return moves;
         }
     }
 }
