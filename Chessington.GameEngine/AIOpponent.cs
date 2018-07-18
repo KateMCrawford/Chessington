65823728e44f5e486c24e3c89bdb7df137a05928
KMC <KMC@GREYREEFSHARK>
2018-07-18 11:58:50 +0100

    HYPER AGRESSION

--- Chessington.GameEngine/AIOpponent.cs
+++ Chessington.GameEngine/AIOpponent.cs
@@ -11,9 +11,10 @@ namespace Chessington.GameEngine
         private static Random rnd = new Random();
         public Player colour = Player.Black;
 
-        private List<Square[]> GetAllMoves (Board board)
+        private List<Square[]> GetAllMoves(Board board)
         {
             List<Square[]> movesList = new List<Square[]>();
+            List<Square[]> aggressiveMovesList = new List<Square[]>();
             for (int i = 0; i < GameSettings.BoardSize; i++)
             {
                 for (int j = 0; j < GameSettings.BoardSize; j++)
@@ -22,18 +23,19 @@ namespace Chessington.GameEngine
                     {
                         foreach (var move in board.GetPiece(Square.At(i, j)).GetAvailableMoves(board))
                         {
-                            movesList.Add(new Square[] { Square.At(i,j), move });
+                            movesList.Add(new Square[] { Square.At(i, j), move });
 
-                            if (board.ContainsOpposingPiece(Square.At(i, j), colour))
+                            if (board.ContainsOpposingPiece(move, colour))
                             {
-                                movesList.Add(new Square[] { Square.At(i, j), move });
-                                movesList.Add(new Square[] { Square.At(i, j), move });
-                                movesList.Add(new Square[] { Square.At(i, j), move });
+                                aggressiveMovesList.Add(new Square[] { Square.At(i, j), move });
                             }
                         }
                     }
                 }
             }
+
+            if (aggressiveMovesList.Any())
+                return aggressiveMovesList;
 
             return movesList;
         }
